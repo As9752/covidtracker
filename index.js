f678cc4ce191f091ca5cced581de85efceaaa189
As9752 <72992208+As9752@users.noreply.github.com>
2021-09-03 00:14:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import  Statewise from "./components/Statewisefolder/Statewise";
+ReactDOM.render(<Statewise/>,document.getElementById("root"));
